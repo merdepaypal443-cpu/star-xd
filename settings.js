d4979596977bb6c03190437c3b6b02853e94d54d
merdepaypal443-cpu <merdepaypal443@gmail.com>
2026-04-22 17:01:37 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 export default {
   // PUT YOUR SESSION ID BELOW (Required)
   // Get from: https://star.malvintech.sbs/
-  SESSION_ID: "",
+  SESSION_ID: "king~hNab0WIf96JO",
 
   // PUT YOUR REMOVE BACKGROUND KEY (Optional)
   // Get from: https://remove.bg/api
@@ -45,7 +45,7 @@ export default {
   BOT_NAME: "sᴛᴀʀ-xᴅ",
 
   // Add Owner WhatsApp Number (without +)
-  OWNER_NUMBER: "263725787000",
+  OWNER_NUMBER: "2250170773242",
 
   // Command Mode: "public" or "private"
   // private = only owner can use commands
